3a4c83aa7ebe42bcce124cee53118dbe1d87dd74
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-04-27 11:37:32 -0300

    ADD melhorias

--- ClubeDaLeitura/Dominio/Emprestimo.cs
+++ ClubeDaLeitura/Dominio/Emprestimo.cs
@@ -1,30 +1,43 @@
-using System;
 using System.Security.Cryptography;
-
 
 namespace ClubeDaLeitura.ConsoleApp.Dominio;
 
 public class Emprestimo
 {
-
     public string Id { get; set; } = string.Empty;
     public Revista Revista { get; set; }
     public Amigo Amigo { get; set; }
-    public DateTime Abertura { get; set; } = DateTime.MinValue;
-    public DateTime Conclusao { get; set; } = DateTime.MinValue;
-    public StatusEmprestimo Status { get; set; } = StatusEmprestimo.Indefinido;
+    public StatusEmprestimo Status { get; set; }
+    public DateTime Abertura { get; set; }
+    public DateTime ConclusaoPrevista
+    {
+        get
+        {
+            int diasDeEmprestimo = Revista.Caixa.DiasDeEmprestimo;
 
+            DateTime conclusao = Abertura.AddDays(diasDeEmprestimo);
+
+            return conclusao;
+        }
+    }
+
+    public bool EstaAtrasado
+    {
+        get
+        {
+            return Status == StatusEmprestimo.Aberto && DateTime.Now > ConclusaoPrevista;
+        }
+    }
 
     public Emprestimo(Revista revista, Amigo amigo)
     {
         Id = Convert
-        .ToHexString(RandomNumberGenerator.GetBytes(20))
-        .ToLower()
-        .Substring(0, 7);
+                .ToHexString(RandomNumberGenerator.GetBytes(20))
+                .ToLower()
+                .Substring(0, 7);
 
         Revista = revista;
         Amigo = amigo;
-
     }
 
     public string[] Validar()
@@ -32,12 +45,26 @@ public class Emprestimo
         string erros = string.Empty;
 
         if (Revista == null)
-            erros += "O campo \"Revista\" deve conter uma revista válida;";
+            erros = "O campo \"Revista\" deve ser preenchido;";
 
         if (Amigo == null)
-            erros += "O campo \"Amigo\" deve conter um amigo válido;";
+            erros = "O campo \"Amigo\" deve ser preenchido;";
 
         return erros.Split(';', StringSplitOptions.RemoveEmptyEntries);
     }
 
+    public void Abrir()
+    {
+        Abertura = DateTime.Now;
+        Status = StatusEmprestimo.Aberto;
+
+        Revista.Emprestar();
+        Amigo.AdicionarEmprestimo(this);
+    }
+
+    public void Concluir()
+    {
+        Status = StatusEmprestimo.Concluido;
+        Revista.Devolver();
+    }
 }
